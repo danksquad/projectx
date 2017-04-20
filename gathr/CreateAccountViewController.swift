bf318fa670ac94c5c3d13bb8e0af128189b4a314
Gates Zeng <gatesazeng@gmail.com>
2017-04-19 20:19:03 -0700

    added create account page

--- gathr/CreateAccountViewController.swift
+++ gathr/CreateAccountViewController.swift
@@ -1,23 +1,24 @@
 //
-//  LoginViewController.swift
+//  CreateAccountViewController.swift
 //  gathr
 //
-//  Created by Gates Zeng on 4/17/17.
+//  Created by Gates Zeng on 4/19/17.
 //  Copyright © 2017 Gates Zeng. All rights reserved.
 //
 
 import UIKit
 import Parse
 
-class LoginViewController: UIViewController {
+class CreateAccountViewController: UIViewController {
+    @IBOutlet weak var fNameField: UITextField!
+    @IBOutlet weak var lNameField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,11 +26,35 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onCreateAccountButton(_ sender: UIButton) {
-
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
-    func displayErrorDialog(error: Error?, sender: UIButton) {
+    @IBAction func onCreateAccount(_ sender: Any) {
+        // create a new PFUser obj
+        let newUser = PFUser()
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        newUser.add(fNameField.text ?? "John", forKey: "firstName")
+        newUser.add(lNameField.text ?? "Smith", forKey: "lastName")
+        
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("created new user")
+                self.performSegue(withIdentifier: "signupSegue", sender: nil)
+            } else {
+                
+                print("Error on Sign Up")
+                self.displayErrorDialog(error: error)
+                
+                print(error?.localizedDescription)
+            }
+        }
+        
+    }
+    
+    func displayErrorDialog(error: Error?) {
         let errorString = (error as! NSError).userInfo["error"] as? NSString
         let signUpErrorController = UIAlertController(title: "Failed", message: "", preferredStyle: .alert)
         
@@ -42,26 +67,7 @@ class LoginViewController: UIViewController {
         signUpErrorController.message = errorString as String!
         self.present(signUpErrorController, animated: true)
     }
-    
-    
-    @IBAction func onLoginButton(_ sender: UIButton) {
-        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!)
-        
-        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: Error?) in
-            if let error = error {
-                self.displayErrorDialog(error: error, sender: sender)
-            } else {
-                print("logged in")
-                if user != nil {
-                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
-                }
-            }
 
-        }
-        
-        
-    }
-    
     /*
     // MARK: - Navigation
 
