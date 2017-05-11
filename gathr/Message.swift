9c05ade375f1c877b7348756b02aec130e1c3258
Gates Zeng <gatesazeng@gmail.com>
2017-05-10 19:21:17 -0700

    updated README

---
+++ gathr/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  gathr
+//
+//  Created by Gates Zeng on 5/10/17.
+//  Copyright © 2017 Gates Zeng. All rights reserved.
+//
+
+import UIKit
+
+class Message: NSObject {
+
+}
