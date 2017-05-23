fb9ee7696e142ca60f08ab2974770793b81ac16b
Jamie S <jamieshi2u@gmail.com>
2017-05-22 21:10:07 -0700

    Added more icons

--- gathr/AppDelegate.swift
+++ gathr/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor(red: 205/255, green: 80/255, blue: 0, alpha: 1.0)
         
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
